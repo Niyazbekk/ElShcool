752ef057da981714d58235f9b47ae12d211eb00d
Niyazbek <55174577+Niyazbekk@users.noreply.github.com>
2021-11-23 03:43:05 +0600

    Delete Source code school directory

--- Source code school/target/classes/db/migration/V1_init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users (
-                       name VARCHAR(50) NOT NULL,
-                       sub_class VARCHAR(100) NOT NULL
-);
-
-INSERT INTO users
-VALUES
-    ('Damir', '3 course'),
-    ('Darkhan', '4 course');
