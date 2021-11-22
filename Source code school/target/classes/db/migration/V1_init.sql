a054ff6d3ab8ba38eb30e057af6d09da47c39d03
Niyazbek <55174577+Niyazbekk@users.noreply.github.com>
2021-11-23 03:42:36 +0600

    Add files via upload

---
+++ Source code school/target/classes/db/migration/V1_init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+                       name VARCHAR(50) NOT NULL,
+                       sub_class VARCHAR(100) NOT NULL
+);
+
+INSERT INTO users
+VALUES
+    ('Damir', '3 course'),
+    ('Darkhan', '4 course');
